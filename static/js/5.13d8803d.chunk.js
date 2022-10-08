3b7afabf4864e0f1c643458b72e93c8667cf7cff
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2022-10-08 17:44:05 +0400

    Updates

---
+++ static/js/5.13d8803d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@roborovski-org/app-react"]=this["webpackJsonp@roborovski-org/app-react"]||[]).push([[5],{542:function(o,p){}}]);
+//# sourceMappingURL=5.13d8803d.chunk.js.map
--- static/js/5.b2430ca6.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@roborovski-org/app-react"]=this["webpackJsonp@roborovski-org/app-react"]||[]).push([[5],{566:function(o,p){}}]);
-//# sourceMappingURL=5.b2430ca6.chunk.js.map
