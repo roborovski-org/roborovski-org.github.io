9e435886d70c2ee15c3d148e433905d969f5411c
an-ivannikov <info@ivannikov.pro>
2023-09-14 01:01:11 +0400

    Updates

--- static/js/5.27f086db.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@roborovski-org/app-react"]=this["webpackJsonp@roborovski-org/app-react"]||[]).push([[5],{534:function(o,p){}}]);
-//# sourceMappingURL=5.27f086db.chunk.js.map
---
+++ static/js/5.4df574d0.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@roborovski-org/app-react"]=this["webpackJsonp@roborovski-org/app-react"]||[]).push([[5],{505:function(o,p){}}]);
+//# sourceMappingURL=5.4df574d0.chunk.js.map
